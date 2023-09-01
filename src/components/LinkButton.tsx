42978dd31058836e798ce674055babde8887b6ff
martapanc <marta.panc@gmail.com>
2023-09-01 15:35:36 +0100

    Center button text

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -10,7 +10,7 @@ const LinkButton = ({link}: LinkProps) => {
 
   return (
     <div className="px-2 py-1">
-      <a className={`button button-${link.buttonStyle} flex flex-row items-center`} role="button"
+      <a className={`button button-${link.buttonStyle} flex flex-row justify-center`} role="button"
          href={link.url} target="_blank" rel="noopener"
       >
         <Image className="inline pt-0 pe-3 pb-1 ps-0" src={`images/icons/${link.icon}`} alt="" width={iconSize} height={iconSize}/>
