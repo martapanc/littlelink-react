3bc5de901715935f07d38905fa95580c280a6b21
martapanc <marta.panc@gmail.com>
2024-10-13 17:04:21 +0200

    Refactor LinkButton component

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -2,28 +2,30 @@ import Image from "next/image";
 import {Link} from "@/types/Link";
 
 interface LinkProps {
-  link: Link
+    link: Link
 }
 
 const LinkButton = ({link}: LinkProps) => {
-  const iconSize = 32;
+    const iconSize = 32;
 
-  return (
-    <div className="px-2 py-1">
-      <a className={`button button-${link.buttonStyle} flex flex-row justify-center`} role="button"
-         href={link.url} target="_blank" rel="noopener"
-      >
-        <Image className="inline pt-0 pe-3 pb-1 ps-0"
-               src={`images/icons/${link.icon}`}
-               alt="Icon"
-               width={iconSize}
-               height={iconSize}
-        />
+    const {buttonStyle, url, icon, title} = link;
 
-        <span>{link.title}</span>
-      </a>
-    </div>
-  );
+    return (
+        <div className="px-2 py-1">
+            <a className={`button button-${buttonStyle} flex flex-row justify-center`} role="button"
+               href={url} target="_blank" rel="noopener" aria-label={title}
+            >
+                <Image className="inline pt-0 pe-3 pb-1 ps-0"
+                       src={`images/icons/${icon}`}
+                       alt={title}
+                       width={iconSize}
+                       height={iconSize}
+                />
+
+                <span>{link.title}</span>
+            </a>
+        </div>
+    );
 }
 
 export default LinkButton;
